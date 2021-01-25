cbde42be3159f1f933fe4346fb3033e4b2d630ad
たた <pf35301@gmail.com>
2021-01-25 18:09:12 +0900

    [WIP]

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,33 +1,80 @@
 use std::ops;
 
-#[derive(Debug)]
-pub struct Vec3<T> {
+#[derive(Debug, Clone, Copy)]
+pub struct Vec3<T>
+where
+    T: Copy,
+{
     x: T,
     y: T,
     z: T,
 }
 
 impl<T> Vec3<T>
-where
-    T: ops::Add,
+where 
+    T: Copy,
 {
     pub fn new(x: T, y: T, z: T) -> Self {
         Vec3 { x, y, z }
     }
 }
 
+impl<T> Vec3<T>
+where
+    T: ops::Add<Output = T> + ops::Mul<Output = T> + Copy + ,
+{
+    //内積
+    pub fn dot(self, rhs: Self) -> T {
+        self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
+    }
+
+    //ノルム^2
+    pub fn sqr_magnitude(self) -> T {
+        self.dot(self)
+    }
+
+    //[TODO] ノルム
+    pub fn magnitude(self) -> T {
+        self.dot(self).sqrt()
+    }
+}
+
+impl<T> Vec3<T>
+where
+    T: ops::Add<Output = T> + ops::Mul<Output = T> + Copy,
+{
+
+}
+
+//外積
+impl<T> Vec3<T>
+where
+    T: ops::Sub<Output = T> + ops::Mul<Output = T> + Copy,
+{
+    //外積
+    pub fn cross(self, rhs: Self) -> Self {
+        Self {
+            x: self.y * rhs.z - self.z * rhs.y,
+            y: self.z * rhs.x - self.x * rhs.z,
+            z: self.x * rhs.y - self.y * rhs.x,
+        }
+    }
+}
+
+//平等性
 impl<T> PartialEq for Vec3<T>
 where
-    T: PartialEq,
+    T: PartialEq + Copy,
 {
     fn eq(&self, rhs: &Self) -> bool {
         self.x == rhs.x && self.y == rhs.y && self.z == rhs.z
     }
 }
 
+//加算
 impl<T> ops::Add<Self> for Vec3<T>
 where
-    T: ops::Add<Output = T>,
+    T: ops::Add<Output = T> + Copy,
 {
     type Output = Self;
 
@@ -40,9 +87,10 @@ where
     }
 }
 
+//減算
 impl<T> ops::Sub<Self> for Vec3<T>
 where
-    T: ops::Sub<Output = T>,
+    T: ops::Sub<Output = T> + Copy,
 {
     type Output = Self;
 
@@ -51,6 +99,61 @@ where
             x: self.x - rhs.x,
             y: self.y - rhs.y,
             z: self.z - rhs.z,
+        }
+    }
+}
+
+//スカラー倍 vec3 * k
+impl<T> ops::Mul<T> for Vec3<T>
+where
+    T: ops::Mul<Output = T> + Copy,
+{
+    type Output = Self;
+
+    fn mul(self, rhs: T) -> Self {
+        Self {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
+    }
+}
+
+//ニュータイプ
+pub struct MultiplableType<T: ops::Mul>(T);
+
+//[TODO]
+//スカラー倍 k * vec3
+impl<T> ops::Mul<Vec3<T>> for MultiplableType<T>
+where
+    T: ops::Mul<Output = T> + Copy,
+{
+    type Output = Vec3<T>;
+
+    fn mul(self, rhs: Vec3<T>) -> Vec3<T> {
+        let Self(value) = self;
+
+        Vec3 {
+            //なにかもっと良い方法はないのか？
+            x: value * rhs.x,
+            y: value * rhs.y,
+            z: value * rhs.z,
+        }
+    }
+}
+
+//アダマール積
+impl<T> ops::Mul<Self> for Vec3<T>
+where
+    T: ops::Mul<Output = T> + Copy,
+{
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self {
+        Self {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+            z: self.z * rhs.z,
         }
     }
 }
