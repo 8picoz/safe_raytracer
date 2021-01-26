4b6ece2853544ac543a6a68b6f8c5c595267fd9f
たた <pf35301@gmail.com>
2021-01-26 17:03:38 +0900

    [WIP]

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,8 +1,23 @@
-use std::vec;
+use raytrace::image::*;
+use raytrace::vec3::*;
 
-use raytrace::vec3;
-use raytrace::image;
 
 fn main() {
-    println!("Hello, world!");
+    let mut image = Image::new(512, 512);
+    let canvas_size = image.get_size();
+
+    for j in 0..canvas_size.1 {
+        for i in 0..canvas_size.0 {
+            image.set_pixel(
+                i,
+                j,
+                Vec3::new_rgb(
+                    i as f32 / canvas_size.0 as f32,
+                    j as f32 / canvas_size.1 as f32,
+                    1.0,
+                ),
+            );
+        }
+    }
+    image.write_ppm("output.ppm").unwrap();
 }
--- src/image.rs
+++ src/image.rs
@@ -3,34 +3,32 @@ use std::io;
 use std::io::prelude::*;
 use std::io::BufWriter;
 
-use num::Float;
-
 use crate::vec3::*;
 
-struct Image {
+pub struct Image {
     width: usize,
     height: usize,
     canvas: Vec<f32>,
-    canvasArraySize: usize,
+    canvas_array_size: usize,
 }
 
 impl Image {
     pub fn new(width: usize, height: usize) -> Self {
-        let arraySize = 3 * width * height;
+        let array_size = 3 * width * height;
 
         Image {
             width: width,
             height: height,
-            canvas: Vec::with_capacity(arraySize as usize),
-            canvasArraySize: arraySize,
+            canvas: vec![0.0; array_size as usize],
+            canvas_array_size: array_size,
         }
     }
 
-    pub fn get_size(&self) -> (&usize, &usize) {
-        (&self.width, &self.height)
+    pub fn get_size(&self) -> (usize, usize) {
+        (self.width, self.height)
     }
 
-    pub fn set_pixel(&mut self, x: usize, y: usize, rgb: &Vec3<f32>) {
+    pub fn set_pixel(&mut self, x: usize, y: usize, rgb: Vec3<f32>) {
         let target_pixel_index = 3 * x + 3 * self.width * y;
 
         self.canvas[target_pixel_index] = rgb.get_x();
@@ -39,7 +37,7 @@ impl Image {
     }
 
     pub fn write_ppm(&self, output_name: &str) -> io::Result<()> {
-        let mut f = File::create(output_name)?;
+        let f = File::create(output_name)?;
         let mut writer = BufWriter::new(f);
 
         writer.write_all(b"P3\r\n")?;
@@ -63,7 +61,7 @@ impl Image {
     }
 
     pub fn gamma_set(&mut self) {
-        for index in 0..self.canvasArraySize {
+        for index in 0..self.canvas_array_size {
             self.canvas[index] = self.canvas[index].powf(1.0 / 2.2);
         }
     }
