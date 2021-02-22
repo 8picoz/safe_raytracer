3733de3635ec982ca2bdb19a2312e27541118c45
たた <pf35301@gmail.com>
2021-02-22 10:28:42 +0900

    [clean] cargo fmt

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -63,7 +63,7 @@ fn raytrace_ssaa(width: u32, height: u32, path: &str, sampling_point: u32) {
 
     let camera = Arc::new(PinholeCamera::new(
         Vec3::new(4.0, 1.0, 7.0),
-         Vec3::from(0.0) - Vec3::new(4.0, 1.0, 7.0).normalized(),
+        Vec3::from(0.0) - Vec3::new(4.0, 1.0, 7.0).normalized(),
         1.0,
     ));
     let image = Arc::new(Mutex::new(image));
@@ -75,24 +75,31 @@ fn raytrace_ssaa(width: u32, height: u32, path: &str, sampling_point: u32) {
         handles.push(thread::spawn(move || {
             for i in 0..width {
                 let mut rng = thread_rng();
-  
-                let kd: Color = (0..sampling_point).map(|_| {
-                    let u = (2.0 * (i as f32 + rng.gen_range(0.0..1.0)) - width as f32) / height as f32;
-                    let v = (2.0 * (j as f32 + rng.gen_range(0.0..1.0)) - height as f32) / height as f32;
 
-                    let ray = camera.make_ray_to_pinhole(u, v);
-                    let raytracer = Raytracer::new(100, &scene);
-                    raytracer.raytrace(ray, 0)
-                }).collect::<Vec<_>>().into_iter().fold(Vec3::from(0.0), |sum, color| sum + color) / sampling_point as f32;
+                let kd: Color = (0..sampling_point)
+                    .map(|_| {
+                        let u = (2.0 * (i as f32 + rng.gen_range(0.0..1.0)) - width as f32)
+                            / height as f32;
+                        let v = (2.0 * (j as f32 + rng.gen_range(0.0..1.0)) - height as f32)
+                            / height as f32;
+
+                        let ray = camera.make_ray_to_pinhole(u, v);
+                        let raytracer = Raytracer::new(100, &scene);
+                        raytracer.raytrace(ray, 0)
+                    })
+                    .collect::<Vec<_>>()
+                    .into_iter()
+                    .fold(Vec3::from(0.0), |sum, color| sum + color)
+                    / sampling_point as f32;
 
                 let kd = Vec3::new(gamma(kd.x), gamma(kd.y), gamma(kd.z));
-                
+
                 let r = num::clamp(kd.x * 255.0, 0.0, 255.0) as u8;
                 let g = num::clamp(kd.y * 255.0, 0.0, 255.0) as u8;
                 let b = num::clamp(kd.z * 255.0, 0.0, 255.0) as u8;
 
                 let mut image = image.lock().unwrap();
-                let pixel = image.get_pixel_mut(i, j); 
+                let pixel = image.get_pixel_mut(i, j);
                 *pixel = image::Rgb([r, g, b]);
             }
         }));
