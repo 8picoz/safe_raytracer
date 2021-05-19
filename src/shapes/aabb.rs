428231020007d449234a69adc338d27897615da6
たた <8picomu@gmail.com>
2021-05-20 06:32:31 +0900

    [clean] inconsistent

--- src/shapes/aabb.rs
+++ src/shapes/aabb.rs
@@ -4,13 +4,13 @@ use crate::vec3::Vec3;
 
 #[allow(clippy::upper_case_acronyms)]
 pub struct AABB {
-    min: Vec3<f32>,
     max: Vec3<f32>,
+    min: Vec3<f32>,
 }
 
 impl AABB {
     pub fn new(max: Vec3<f32>, min: Vec3<f32>) -> Self {
-        AABB { max, min }
+        Self { max, min }
     }
 
     pub fn new_max_bound() -> Self {
@@ -18,6 +18,6 @@ impl AABB {
         let max = Vec3::from(f32::MAX);
         let min = Vec3::from(f32::MIN);
 
-        AABB { max, min }
+        Self { max, min }
     }
 }
