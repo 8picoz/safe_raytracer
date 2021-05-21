4330baf36b5fb76290a7282724b7815c9381de60
たた <8picomu@gmail.com>
2021-05-21 19:08:46 +0900

    [add] merge_(aabb|point), center

--- src/shapes/aabb.rs
+++ src/shapes/aabb.rs
@@ -3,12 +3,12 @@ use core::f32;
 use crate::vec3::Vec3;
 
 #[allow(clippy::upper_case_acronyms)]
-pub struct AABB {
-    max: Vec3<f32>,
-    min: Vec3<f32>,
+pub struct AABB<T: Copy + Send + Sync> {
+    max: Vec3<T>,
+    min: Vec3<T>,
 }
 
-impl AABB {
+impl AABB<f32> {
     pub fn new(max: Vec3<f32>, min: Vec3<f32>) -> Self {
         Self { max, min }
     }
@@ -19,5 +19,29 @@ impl AABB {
         let min = Vec3::from(f32::MIN);
 
         Self { max, min }
+    }
+    
+    pub fn new_min_bound() -> Self {
+        let zero = Vec3::from(0.0);
+
+        Self { max: zero, min: zero }
+    }
+
+    pub fn merge_aabb(&self, other: Self) -> Self {
+        let max = self.max.cmp_each_max_value(other.max);
+        let min = self.min.cmp_each_min_value(other.min);
+
+        AABB::new(max, min)
+    }
+
+    pub fn merge_point(&self, other: Vec3<f32>) -> Self {
+        let max = self.max.cmp_each_max_value(other);
+        let min = self.min.cmp_each_min_value(other);
+
+        AABB::new(max, min)
+    }
+
+    pub fn center(&self) -> Vec3<f32> {
+        (self.max * self.min) * 0.5
     }
 }
