95c7131d771698cbaf3b9279845879e16e1c8457
tata <8picoz@tata.pw>
2021-10-15 17:42:47 +0900

    wip

---
+++ src/matrix.rs
@@ -0,0 +1,44 @@
+pub struct Matrix2x2<T> {
+    pub x0y0: T,
+    pub x0y1: T,
+    pub x1y0: T,
+    pub x1y1: T,
+}
+
+pub struct Matrix3x3<T> {
+    pub x0y0: T,
+    pub x0y1: T,
+    pub x0y2: T,
+    pub x1y0: T,
+    pub x1y1: T,
+    pub x1y2: T,
+    pub x2y0: T,
+    pub x2y1: T,
+    pub x2y2: T,
+}
+
+pub struct Matrix4x4<T> {
+    pub x0y0: T,
+    pub x0y1: T,
+    pub x0y2: T,
+    pub x0y3: T,
+    pub x1y0: T,
+    pub x1y1: T,
+    pub x1y2: T,
+    pub x1y3: T,
+    pub x2y0: T,
+    pub x2y1: T,
+    pub x2y2: T,
+    pub x2y3: T,
+    pub x3y0: T,
+    pub x3y1: T,
+    pub x3y2: T,
+    pub x3y3: T,
+}
+
+
+pub impl Matrix {
+    pub fn new() -> Self {
+
+    }
+}
