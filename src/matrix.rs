4620b0df0b1e51077eeac7e3396a13ade30e1f1e
8picoz <8picoz@tata.pw>
2021-10-20 20:22:31 +0900

    [refactor] swap position x and y

--- src/matrix.rs
+++ src/matrix.rs
@@ -8,8 +8,8 @@ where
     T: Bound<T>,
 {
     pub x0y0: T,
-    pub x0y1: T,
     pub x1y0: T,
+    pub x0y1: T,
     pub x1y1: T,
 }
 
@@ -18,13 +18,13 @@ where
     T: Bound<T>,
 {
     pub x0y0: T,
-    pub x0y1: T,
-    pub x0y2: T,
     pub x1y0: T,
-    pub x1y1: T,
-    pub x1y2: T,
     pub x2y0: T,
+    pub x0y1: T,
+    pub x1y1: T,
     pub x2y1: T,
+    pub x0y2: T,
+    pub x1y2: T,
     pub x2y2: T,
 }
 
@@ -33,34 +33,30 @@ where
     T: Bound<T>,
 {
     pub x0y0: T,
-    pub x0y1: T,
-    pub x0y2: T,
-    pub x0y3: T,
     pub x1y0: T,
-    pub x1y1: T,
-    pub x1y2: T,
-    pub x1y3: T,
     pub x2y0: T,
-    pub x2y1: T,
-    pub x2y2: T,
-    pub x2y3: T,
     pub x3y0: T,
+    pub x0y1: T,
+    pub x1y1: T,
+    pub x2y1: T,
     pub x3y1: T,
+    pub x0y2: T,
+    pub x1y2: T,
+    pub x2y2: T,
     pub x3y2: T,
+    pub x0y3: T,
+    pub x1y3: T,
+    pub x2y3: T,
     pub x3y3: T,
 }
-
-
-
-
 
 impl<T> Matrix2x2<T> 
 where 
     T: Bound<T>,
 {
-    pub fn new(x0y0: T, x0y1: T, x1y0: T, x1y1: T) -> Self
+    pub fn new(x0y0: T, x1y0: T, x0y1: T, x1y1: T) -> Self
     {
-        Matrix2x2 { x0y0, x0y1, x1y0, x1y1 }
+        Matrix2x2 { x0y0, x1y0, x0y1, x1y1 }
     }    
 }
 
@@ -71,8 +67,8 @@ where
     type Output = Self;
 
     fn add(self, other: Self) -> Self {
-        Matrix2x2::new(self.x0y0 + other.x0y0, self.x0y1 + other.x0y1, 
-                        self.x1y0 + other.x1y0, self.x1y1 + other.x1y1)
+        Matrix2x2::new(self.x0y0 + other.x0y0, self.x1y0 + other.x1y0, 
+                        self.x0y1 + other.x0y1, self.x1y1 + other.x1y1)
     }
 }
 
@@ -81,9 +77,9 @@ where
     T: Bound<T>,
 {
     #[allow(clippy::too_many_arguments)]
-    pub fn new(x0y0: T, x0y1: T, x0y2: T, x1y0: T, x1y1: T, x1y2: T, x2y0: T, x2y1: T, x2y2: T) -> Self
+    pub fn new(x0y0: T, x1y0: T, x2y0: T, x0y1: T, x1y1: T, x2y1: T, x0y2: T, x1y2: T, x2y2: T) -> Self
     {
-        Matrix3x3 { x0y0, x0y1, x0y2, x1y0, x1y1, x1y2, x2y0, x2y1, x2y2 }
+        Matrix3x3 { x0y0, x1y0, x2y0, x0y1, x1y1, x2y1, x0y2, x1y2, x2y2 }
     }
 }
 
@@ -94,9 +90,9 @@ where
     type Output = Self;
 
     fn add(self, other: Self) -> Self {
-        Matrix3x3::new(self.x0y0 + other.x0y0, self.x0y1 + other.x0y1, self.x0y2 + other.x0y2,
-                        self.x1y0 + other.x1y0, self.x1y1 + other.x1y1, self.x1y2 + other.x1y2,
-                        self.x2y0 + other.x2y0, self.x2y1 + other.x2y1, self.x2y2 + other.x2y2)
+        Matrix3x3::new(self.x0y0 + other.x0y0, self.x1y0 + other.x1y0, self.x2y0 + other.x2y0,
+                        self.x0y1 + other.x0y1, self.x1y1 + other.x1y1, self.x2y1 + other.x2y1,
+                        self.x0y2 + other.x0y2, self.x1y2 + other.x1y2, self.x2y2 + other.x2y2)
     }
 }
 
@@ -105,9 +101,9 @@ where
     T: Bound<T>,
 {
     #[allow(clippy::too_many_arguments)]
-    pub fn new(x0y0: T, x0y1: T, x0y2: T, x0y3: T, x1y0: T, x1y1: T, x1y2: T, x1y3: T, x2y0: T, x2y1: T, x2y2: T, x2y3: T, x3y0: T, x3y1: T, x3y2: T, x3y3: T) -> Self
+    pub fn new(x0y0: T, x1y0: T, x2y0: T, x3y0: T, x0y1: T, x1y1: T, x2y1: T, x3y1: T, x0y2: T, x1y2: T, x2y2: T, x3y2: T, x0y3: T, x1y3: T, x2y3: T, x3y3: T) -> Self
     {
-        Matrix4x4 { x0y0, x0y1, x0y2, x0y3, x1y0, x1y1, x1y2, x1y3, x2y0, x2y1, x2y2, x2y3, x3y0, x3y1, x3y2, x3y3 }
+        Matrix4x4 { x0y0, x1y0, x2y0, x3y0, x0y1, x1y1, x2y1, x3y1, x0y2, x1y2, x2y2, x3y2, x0y3, x1y3, x2y3, x3y3 }
     }
 }
 
@@ -118,9 +114,9 @@ where
     type Output = Self;
 
     fn add(self, other: Self) -> Self {
-        Matrix4x4::new(self.x0y0 + other.x0y0, self.x0y1 + other.x0y1, self.x0y2 + other.x0y2, self.x0y3 + other.x0y3,
-                        self.x1y0 + other.x1y0, self.x1y1 + other.x1y1, self.x1y2 + other.x1y2, self.x1y3 + other.x1y3,
-                        self.x2y0 + other.x2y0, self.x2y1 + other.x2y1, self.x2y2 + other.x2y2, self.x2y3 + other.x2y3,
-                        self.x3y0 + other.x3y0, self.x3y1 + other.x3y1, self.x3y2 + other.x3y2, self.x3y3 + other.x3y3)
+        Matrix4x4::new(self.x0y0 + other.x0y0, self.x1y0 + other.x1y0, self.x2y0 + other.x2y0, self.x3y0 + other.x3y0,
+                        self.x0y1 + other.x0y1, self.x1y1 + other.x1y1, self.x2y1 + other.x2y1, self.x3y1 + other.x3y1,
+                        self.x0y2 + other.x0y2, self.x1y2 + other.x1y2, self.x2y2 + other.x2y2, self.x3y2 + other.x3y2,
+                        self.x0y3 + other.x0y3, self.x1y3 + other.x1y3, self.x2y3 + other.x2y3, self.x3y3 + other.x3y3)
     }
 }
